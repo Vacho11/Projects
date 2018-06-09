fa94b76ed6c625b19efdd849f29ae506e156bf53
Valeri Mdivani <Mdivanivacho@gmail.com>
2018-06-09 14:38:16 +0300

    Delete project.py

--- project.py
+++
@@ -1,9 +0,0 @@
-import matplotlib.pyplot as plt
-import numpy as np
-import pandas as pd
-
-file = input('The name of the file:', )
-data = pd.ExcelFile(file)
-#data1 = data.parse(0)
-
-#print(data1[0, 0])
